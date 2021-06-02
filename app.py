3d4523dc68fde0869ac0541b69bbb0ec490586be
prudhvirajboddu <prudhvirajboddu@gmail.com>
2021-06-02 06:06:14 +0530

    From tflite running the inference

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-class FixedDropout(tf.keras.layers.Dropout): # for tpu trained model to work in cpu for deployment
-  def _get_noise_shape(self, inputs):
-    if self.noise_shape is None:
-        return self.noise_shape
-    symbolic_shape = K.shape(inputs)
-    noise_shape = [symbolic_shape[axis] if shape is None else shape for axis, shape in enumerate(self.noise_shape)]
-    return tuple(noise_shape)
-
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from tensorflow.python.keras.backend import dtype
@@ -26,17 +18,25 @@ app = Flask(__name__)
 
 model=None
 
-def model_predict(img_path, model):
+def model_predict(img_path, interpreter):
     img = image.load_img(img_path, target_size=(500,500))
 
     # Preprocessing the image
-    x = image.img_to_array(img)
-    x = x.astype("float") / 255.0 #normailizing the image between [0-255]
-    x = np.expand_dims(x,0) #adding batch size of 1 to get shape (1,img_height,img_width,channels=3)
+    inp_tensor = image.img_to_array(img,dtype=np.float32)
+    inp_tensor = inp_tensor / 255.0 #normailizing the image between [0-255]
+    inp_tensor = np.expand_dims(inp_tensor,0) #adding batch size of 1 to get shape (1,img_height,img_width,channels=3)
     # print(x.shape)
 
-    preds = model.predict(x)
-    return preds
+    input_index = interpreter.get_input_details()[0]["index"]
+    interpreter.set_tensor(input_index, inp_tensor)
+    interpreter.invoke()
+    output_details = interpreter.get_output_details()
+
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    results = np.squeeze(output_data)
+
+    # print(results)
+    return results
 
 
 @app.route('/', methods=['GET'])
@@ -57,10 +57,10 @@ def upload():
         f.save(file_path)
 
         # Make prediction
-        preds = model_predict(file_path, model)
+        preds = model_predict(file_path, interpreter)
 
         os.remove(file_path)
-        if preds[0][0]>=0.5:
+        if preds>=0.5:
             return "Melanoma Lesion"#+"\nconfidence :"+format(preds[0][0]*100,'.2f')+"%"
         else:
             return "Non-Melanoma Lesion"#+"\nconfidence :"+format(preds[0][0]*100,'.2f')+"%"
@@ -68,7 +68,10 @@ def upload():
 
 
 if __name__ == '__main__':
-    model=load_model('b6structure.h5',custom_objects={'FixedDropout':FixedDropout},compile=False)
+    interpreter=tf.lite.Interpreter('model.tflite')
+    interpreter.allocate_tensors()
+    inputs=interpreter.get_input_details()
+    outputs = interpreter.get_output_details()
     #just need to change the model if it is efficientnet trained on tpu add custom objects code 
     app.run(host="0.0.0.0", port=5000, debug=True)
     # http_server=WSGIServer(('0.0.0.0',5000),app)
